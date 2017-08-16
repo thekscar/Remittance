9ed71cc0229fe15ac8c72b78b3e5971f786ca42a
Karen <kscarbrough1@gmail.com>
2017-08-16 17:14:48 -0500

    Delete 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Splitter = artifacts.require("./Splitter.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Splitter);
-  //What exactly does link do? 
-  // deployer.link(ConvertLib, MetaCoin);
-};
